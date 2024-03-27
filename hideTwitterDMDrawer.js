a554e10fcf28bd1a77476b5cf54f52a346f0c656
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-27 14:28:29 -0700

    Remove unnecessary parts of this code

--- hideTwitterDMDrawer.js
+++
@@ -1,4 +0,0 @@
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = '[data-testid="DMDrawer"] { display: none !important; }';
-document.head.appendChild(style);
